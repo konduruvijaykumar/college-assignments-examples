54913a506e40209368219980c3f45c423d0b7a75
Vijay Konduru <konduruvijaykumar@gmail.com>
2019-08-18 15:49:10 +0530

    initial commit

---
+++ assignment1/src/main/java/org/pjay/student/Student.java
@@ -0,0 +1,14 @@
+package org.pjay.student;
+
+/**
+ * @author vijayk
+ */
+public class Student {
+    protected String name;
+    protected int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
