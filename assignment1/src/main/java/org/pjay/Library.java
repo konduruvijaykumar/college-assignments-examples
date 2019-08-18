fe0c3b8938fb658e59bfd227b3aa443ee6843259
Vijay Konduru <konduruvijaykumar@gmail.com>
2019-08-18 16:03:49 +0530

    initial commit

---
+++ assignment1/src/main/java/org/pjay/Library.java
@@ -0,0 +1,18 @@
+package org.pjay;
+
+/**
+ * @author vijayk
+ */
+public class Library {
+
+    int bookNo;
+
+    void getBookNo(int bookNo){
+
+    }
+
+    void displayBookNo(){
+        System.out.println("bookNo :: " + bookNo);
+    }
+
+}
