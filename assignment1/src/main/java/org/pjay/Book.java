a4f910e4bf2f14ccd3fa2299358ac16b9e94c224
Vijay Konduru <konduruvijaykumar@gmail.com>
2019-08-18 16:55:11 +0530

    initial commit

--- assignment1/src/main/java/org/pjay/Book.java
+++ assignment1/src/main/java/org/pjay/Book.java
@@ -9,7 +9,8 @@ public class Book extends Library {
     String title;
 
     void getInfo(String author, String title) {
-
+        // No idea on this functionality method, i only saw the pdf of questions as assignment.
+        // Nothing was mentioned on this. Students from class should have idea of it.
     }
 
     void displayAuthorAndTitle() {
--- assignment1/src/main/java/org/pjay/Library.java
+++ assignment1/src/main/java/org/pjay/Library.java
@@ -8,7 +8,8 @@ public class Library {
     int bookNo;
 
     void getBookNo(int bookNo) {
-
+        // No idea on this functionality method, i only saw the pdf of questions as assignment.
+        // Nothing was mentioned on this. Students from class should have idea of it.
     }
 
     void displayBookNo() {
