665d331312c558c65577d2000fa67f0d39c5b11f
Vijay Konduru <konduruvijaykumar@gmail.com>
2019-08-18 15:54:10 +0530

    initial commit

---
+++ assignment1/src/main/java/org/pjay/StudentTest.java
@@ -0,0 +1,15 @@
+package org.pjay;
+
+import org.pjay.studetails.StudentDetails;
+
+/**
+ * @author vijayk
+ */
+public class StudentTest {
+
+    public static void main(String[] args) {
+        StudentDetails studentDetails = new StudentDetails('M');
+        studentDetails.showData();
+    }
+
+}
